07a7645ef42061bc100d7e00fc5cbac3d1223aad
DPEDITS <debashishparida75@gmail.com>
2026-02-08 23:10:34 +0530

    Done backend changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,13 +3,23 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fularani-frontend.onrender.com"
+];
+
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-  }),
+  })
 );
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
